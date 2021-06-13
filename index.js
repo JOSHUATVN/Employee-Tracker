ab9a7cfce34cb878755d6738d83b17352fdadc29
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-06-13 15:39:19 -0500

    saving work, added emplyee roles function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql2 = require("mysql2");
 const inquirer = require("inquirer");
+const { restoreDefaultPrompts } = require("inquirer");
 
 const connection = mysql2.createConnection({
   host: "localhost",
@@ -58,34 +59,195 @@ function userPrompt() {
 }
 
 function displayEmployees() {
-  let query = "SELECT employees.first_name, employees.last_name, roles.title, roles.salary, department.department_name AS department, employees.manager_id " +
-  "FROM employees " +
-  "JOIN roles ON roles.id = employees.role_id " +
-  "JOIN department ON roles.department_id = department.id " +
-  "ORDER BY employees.id;";
+  let query =
+    "SELECT employees.first_name, employees.last_name, roles.title, roles.salary, department.department_name AS department, employees.manager_id " +
+    "FROM employees " +
+    "JOIN roles ON roles.id = employees.role_id " +
+    "JOIN department ON roles.department_id = department.id " +
+    "ORDER BY employees.id;";
 
   connection.query(query, function (err, res) {
     if (err) throw err;
-    for(i = 0; i < res.length; i++) {
-      if(res[i].manager_id === 0) {
-        res[i].manager_id = "Null"
+    for (i = 0; i < res.length; i++) {
+      if (res[i].manager_id === 0) {
+        res[i].manager_id = "Null";
       } else {
-        res[i].manager = res[res[i].manager_id - 1]. first_name + " " + res[res[i].manager_id - 1].last_name;
-      };
+        res[i].manager =
+          res[res[i].manager_id - 1].first_name +
+          " " +
+          res[res[i].manager_id - 1].last_name;
+      }
       delete res[i].manager_id;
-    };
+    }
     console.table(res);
     userPrompt();
   });
-};
-
-function displayRoles() {
-  let query = "SELECT department.department_name AS departments FROM department;";
 }
 
-function displayDepartment() {}
+function displayRoles() {
+  let query =
+    "SELECT roles.title, roles.salary, department.department_name AS department FROM roles INNER JOIN department ON department.id = roles.department_id;";
 
-function addEmployee() {}
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    userPrompt();
+  });
+}
+
+function displayDepartment() {
+  let query =
+    "SELECT department.department_name AS departments FROM department;";
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    userPrompt();
+  });
+}
+
+function addEmployee() {
+  let query = "SELECT title FROM roles";
+  let query1 =
+    "SELECT employees.first_name, employees.last_name, roles.title, roles.salary, department.department_name, employees.manager_id " +
+    "FROM employees " +
+    "JOIN roles ON roles.id = employees.role_id " +
+    "JOIN department ON roles.department_id = department.id " +
+    "ORDER BY employees.id;";
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    let listOfRoles = res;
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+      for (i = 0; i < res.length; i++) {
+        if (res[i].manager_id === 0) {
+          res[i].manager = " Null";
+        } else {
+          res[i].manager =
+            res[res[i].manager_id - 1].first_name +
+            " " +
+            res[res[i].manager_id - 1].last_name;
+        }
+        delete res[i].manager_id;
+      }
+      console.table(res);
+
+      let listOfManagers = res;
+      let addEmployeePrompt = [
+        {
+          name: "first_name",
+          type: "input",
+          message: "Enter employee's first name",
+        },
+        {
+          name: "last_name",
+          type: "input",
+          message: "Enter employee's last name",
+        },
+        {
+          name: "select_role",
+          type: "list",
+          message: "Select employee's role",
+
+          choices: function () {
+            roles = [];
+            for (i = 0; i < listOfRoles.length; i++) {
+              const rolesID = i + 1;
+              roles.push(rolesID + ": " + listOfRoles[i].title);
+            }
+            roles.unshift("0: EXIT");
+            return roles;
+          },
+        },
+
+        {
+          name: "select_manager",
+          type: "list",
+          message: "Select employee's manager",
+
+          choices: function () {
+            managers = [];
+            for (i = 0; i < listOfManagers.length; i++) {
+              const managerID = i + 1;
+              managers.push(
+                managerID +
+                  ": " +
+                  listOfManagers[i].first_name +
+                  " " +
+                  listOfManagers[i].last_name
+              );
+            }
+            managers.unshift("0: Null");
+            manager.unshift("E: EXIT");
+            return managers;
+          },
+          when: function () {
+            return results.select_role !== "0: EXIT";
+          },
+        },
+      ];
+      inquirer.prompt(addEmployeePrompt).then(function (result) {
+        if (
+          result.select_role === "0: EXIT" ||
+          result.select_manager === "E: EXIT"
+        ) {
+          userPrompt();
+        } else {
+          console.log(result);
+          let query = "INSERT INTO employees SET ?";
+          connection.query(
+            query,
+            {
+              first_name: result.first_name,
+              last_name: result.last_name,
+              role_id: parseInt(result.select_role.split(":")[0]),
+            },
+            function (err, res) {
+              if (err) throw err;
+            }
+          );
+          addMorePrompt = [
+            {
+              name: "add_more",
+              type: "list",
+              message: "Would you like to add another employee?",
+              choices: ["YES", "EXIT"],
+            },
+          ];
+          inquirer.prompt(addMorePrompt).then(function (result) {
+            let query =
+              "SELECT employees.first_name, employees.last_name, roles.title, roles.salary, department.department_name, employees.manager_id " +
+              "FROM employees " +
+              "JOIN roles ON roles.id = employees.role_id " +
+              "JOIN department ON roles.department_id = department.id " +
+              "ORDER BY employees.id;";
+
+            connection.query(query, function (err, res) {
+              if (err) throw err;
+              if (result.add_more === "Yes") {
+                addEmployee();
+              } else if (result.add_more === "EXIT") {
+                for (i = 0; i < res.length; i++) {
+                  if (res[i].manager_id === 0) {
+                    res[i].manager = "Null";
+                  } else {
+                    res[i].manager =
+                      res[res[i].manager_id - 1].first_name +
+                      " " +
+                      res[res[i].manager_id - 1].last_name;
+                  }
+                  delete res[i].manager_id;
+                }
+                console.table(res);
+                userPrompt();
+              }
+            });
+          });
+        }
+      });
+    });
+  });
+};
 
 function addRole() {}
 
