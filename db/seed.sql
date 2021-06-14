3f620c1e94c4072ee5b236e83b448d582241e710
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-06-13 22:41:00 -0500

    having problems w sql, moved sql files into a db file and removed some data from seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,18 @@
+USE employee_db;
+
+INSERT INTO department(department_name)
+VALUES
+
+
+
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES
+
+
+INSERT INTO roles(title, salary, department_id)
+VALUES
+
+
+
+
+
